df8691d6209144d2c5df8f081f3854fdd557478e
Fael landal <rafaelcruzpaes@gmail.com>
2024-04-04 12:12:58 -0300

    fix/clasYoutube

--- YouTube/YouTube.py
+++ YouTube/YouTube.py
@@ -1,19 +1,15 @@
+from pytube import YouTube as YT
 import os
-import io
 import requests
 import logging
 from mutagen.mp4 import MP4, MP4Cover
 from PIL import Image
 from model.music import Music
 from typing import List
+import io
 from dotenv import load_dotenv
-from pytube import YouTube as YT
 
 class Result:
-    """
-    Representa um resultado de pesquisa do YouTube.
-    """
-
     def __init__(self, title: str, thumb: str, url: str, author: str):
         self.title = title
         self.thumb = thumb
@@ -21,9 +17,6 @@ class Result:
         self.author = author
 
     def __str__(self) -> str:
-        """
-        Retorna uma representação em string do resultado.
-        """
         content = (
             f"    url: {self.url},\n    thumb: {self.thumb}\n    title: {self.title}"
         )
@@ -32,43 +25,58 @@ class Result:
 class YouTube:
     """
     Classe para interação com a API do YouTube.
+    
+    Attributes:
+        api_key (str): A chave de API do YouTube.
     """
-
     def __init__(self) -> None:
         """
-        Inicializa a classe, carregando a chave de API do arquivo .env.
+        Inicializa a classe YouTube e carrega a chave de API do YouTube do ambiente.
         """
         load_dotenv()
         self.api_key = os.getenv("API_KEY")
 
-    def download(self, id: str) -> bytes:
+    def set_content(self, music: Result) -> None:
         """
-        Realiza o download de um vídeo do YouTube e retorna o conteúdo do arquivo de áudio.
+        Define o título, imagem de thumbnail, URL e autor de um vídeo.
 
         Args:
-            id (str): O ID do vídeo do YouTube.
+            music (Result): O objeto Result contendo informações sobre o vídeo.
+        """
+        self.title = music.title
+        self.thumb = music.thumb
+        self.url = music.url
+        self.author = music.author
+
+    def download(self, id: str) -> bytes:
+        """
+        Baixa o áudio de um vídeo do YouTube e adiciona metadados ao arquivo MP4.
+
+        Args:
+            id (str): O ID do vídeo no YouTube.
 
         Returns:
-            bytes: O conteúdo do arquivo de áudio.
+            bytes: O conteúdo do áudio baixado.
         """
         path = ""
         try:
             yt = YT(f"https://music.youtube.com/watch?v={id}")
             video = yt.streams.filter(only_audio=True).first()
-            result = Result(yt.title, yt.thumbnail_url, yt.video_id, yt.author)
-            path = video.download("cache")
+            self.set_content(Result(yt.title, yt.thumbnail_url, yt.video_id, yt.author))
+            downloaded_file = video.download("cache")
+            path = downloaded_file
 
-            response = requests.get(result.thumb)
+            response = requests.get(yt.thumbnail_url)
             image = Image.open(io.BytesIO(response.content))
             byte_arr = io.BytesIO()
             image.save(byte_arr, format="JPEG")
             mp4_cover = MP4Cover(byte_arr.getvalue(), imageformat=MP4Cover.FORMAT_JPEG)
-            
+
             mp4 = MP4(path)
             mp4["covr"] = [mp4_cover]
-            mp4["\xa9ART"] = result.author
-            mp4["\xa9alb"] = f"thumbnail_url = {result.thumb}, url = {id}"
-            mp4["\xa9nam"] = result.title
+            mp4["\xa9ART"] = yt.author
+            mp4["\xa9alb"] = f"thumbnail_url = {yt.thumbnail_url}, url = {id}"
+            mp4["\xa9nam"] = yt.title
             mp4["\xa9des"] = id
             mp4.save()
 
@@ -80,15 +88,15 @@ class YouTube:
             logging.error(f"Error while downloading the video: {e}")
             return None
         finally:
-            if path != "":
+            if path:
                 os.remove(path)
 
     def search(self, query: str) -> List[Result]:
         """
-        Realiza uma busca no YouTube com base em uma consulta e retorna os resultados.
+        Realiza uma busca por vídeos no YouTube com base em uma query.
 
         Args:
-            query (str): A consulta de busca.
+            query (str): O termo de busca.
 
         Returns:
             List[Result]: Uma lista de resultados da busca.
@@ -104,21 +112,119 @@ class YouTube:
             response = requests.get(base_url, params=params)
             results = response.json()
 
-            videos: List[Result] = []
-            for item in results.get("items", []):
-                if "id" in item and "videoId" in item["id"]:
-                    video_title = item["snippet"]["title"]
-                    video_thumb = item["snippet"]["thumbnails"].get("high", {}).get("url")
-                    video_url = item["id"]["videoId"]
-                    video_author = item["snippet"]["channelTitle"]
-                    video = Result(video_title, video_thumb, video_url, video_author)
-                    videos.append(video)
-                else:
-                    logging.warning(f"Skipping non-video item: {item}")
-            return videos
+            if "items" in results:
+                videos = []
+                for item in results.get("items", []):
+                    if "id" in item and "videoId" in item["id"]:
+                        video_title = item.get("snippet", {}).get("title")
+                        video_thumb = item.get("snippet", {}).get("thumbnails", {}).get("high", {}).get("url")
+                        video_url = item["id"]["videoId"]
+                        video_author = item.get("snippet", {}).get("channelTitle")
+                        video = Result(video_title, video_thumb, video_url, video_author)
+                        videos.append(video)
+                    else:
+                        logging.warning(f"Skipping non-video item: {item}")
+                return videos
+            else:
+                logging.error(f"No 'items' key in API response: {results}")
+                return []
+        except requests.RequestException as e:
+            logging.error(f"Request to YouTube API failed: {e}")
+            return []
         except Exception as e:
             logging.error(f"An unexpected error occurred: {e}")
             return []
 
-    # Métodos adicionais...
+    def get_videos(self, playlist_id: str) -> List[Music]:
+        """
+        Obtém os vídeos de uma playlist do YouTube.
 
+        Args:
+            playlist_id (str): O ID da playlist no YouTube.
+
+        Returns:
+            List[Music]: Uma lista de vídeos da playlist.
+        """
+        try:
+            base_url = "https://www.googleapis.com/youtube/v3/playlistItems"
+            params = {
+                "part": "snippet",
+                "maxResults": 25,
+                "playlistId": playlist_id,
+                "key": self.api_key,
+            }
+            response = requests.get(base_url, params=params)
+            results = response.json()
+
+            videos = []
+            for item in results.get("items", []):
+                video_title = item["snippet"]["title"]
+                video_thumb = item["snippet"]["thumbnails"].get("high", {}).get("url")
+                video_url = item.get("snippet").get("resourceId").get("videoId")
+                video_author = item["snippet"]["videoOwnerChannelTitle"]
+                music = Music(video_title, video_thumb, video_url, video_author)
+                videos.append(music)
+
+            return videos
+        except Exception as e:
+            logging.error(f"Error while downloading the video: {e}")
+            return None
+
+    def get_playlist(self, query: str) -> List[Result]:
+        """
+        Obtém playlists do YouTube com base em uma query.
+
+        Args:
+            query (str): O termo de busca.
+
+        Returns:
+            List[Result]: Uma lista de playlists encontradas.
+        """
+        try:
+            base_url = "https://www.googleapis.com/youtube/v3/search"
+            params = {
+                "part": "snippet",
+                "maxResults": 25,
+                "key": self.api_key,
+                "type": "playlist",
+                "q": query,
+            }
+            response = requests.get(base_url, params=params)
+            results = response.json()
+
+            if "items" in results:
+                videos = []
+                for item in results.get("items", []):
+                    if "id" in item and "playlistId" in item["id"]:
+                        video_title = item.get("snippet", {}).get("title")
+                        video_thumb = item.get("snippet", {}).get("thumbnails", {}).get("high", {}).get("url")
+                        video_url = item["id"]["playlistId"]
+                        video_author = item.get("snippet", {}).get("channelTitle")
+                        video = Result(video_title, video_thumb, video_url, video_author)
+                        videos.append(video)
+                    else:
+                        logging.warning(f"Skipping non-video item: {item}")
+                return videos
+            else:
+                logging.error(f"No 'items' key in API response: {results}")
+                return []
+        except requests.RequestException as e:
+            logging.error(f"Request to YouTube API failed: {e}")
+            return []
+        except Exception as e:
+            logging.error(f"An unexpected error occurred: {e}")
+            return []
+
+    def stream(self, id: str) -> str:
+        """
+        Retorna a URL de streaming de áudio de um vídeo do YouTube.
+
+        Args:
+            id (str): O ID do vídeo no YouTube.
+
+        Returns:
+            str: A URL de streaming do áudio.
+        """
+        yt = YT(f"https://music.youtube.com/watch?v={id}")
+        audio = yt.streams.filter(only_audio=True).first()
+        return audio.url
